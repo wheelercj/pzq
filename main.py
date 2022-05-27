9728b87f81a96c483944c0bd3a16b8603c99fe95
wheelercj <chrisw289@gmail.com>
2022-05-26 20:02:55 -0700

    Make the main imports conditional and attempt to save on exceptions

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 import sys
-from zq import tui, gui
 
 
 def main():
-    if len(sys.argv) > 1 and sys.argv[1] == "gui":
-        gui.main()
-    else:
-        tui.main()
+    try:
+        if len(sys.argv) > 1 and sys.argv[1] == "gui":
+            from zq import gui
+            gui.main()
+        else:
+            from zq import tui
+            tui.main()
+    except Exception as e:
+        from zq.settings import save_settings
+        save_settings()
+        raise e
 
 
 if __name__ == "__main__":
